240212a73d94b1cb36ca5b61b19311c030441d11
Tijiena <a13816106692@163.com>
2023-04-21 10:56:11 -0400

    .

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const btnEl = document.getElementById('btn')
 const textEl=document.getElementById('text')
+const canvas=document.getElementById('drawing-board');
+const toolbar=document.getElementById('toolbar');
+const ctx=canvas.getContext('2d');
+
+const canvasOffsetX=canvas.offsetLeft;
+const canvasOffsetY=canvas.offsetTop;
+
+canvas.width=window.innerWidth-canvasOffsetX;
+canvas.height=window.innerHeight-canvasOffsetY;
 
 const apikey="sRN2BH0Xmvjz92FfSTwpuw==lWvoYpamYJ0IjO3G";
  
@@ -8,9 +17,11 @@ const apikey="sRN2BH0Xmvjz92FfSTwpuw==lWvoYpamYJ0IjO3G";
 
 const options={
     method:"GET",
+    contentType:"verb",
     headers:{
         "X-Api-Key":apikey,
     }
+    
 
 };
   
@@ -24,3 +35,46 @@ const options={
  
  
     btnEl.addEventListener("click",getText);
+
+    let isPainting=false;
+    let lineWidth=5;
+
+    toolbar.addEventListener('click',e=>{
+        if(e.target.id==='clear'){
+            ctx.clearRect(0,0,canvas.width,canvas.height);}
+        });
+    toolbar.addEventListener('change',e=>{
+        if(e.target.id==='stroke'){
+            ctx.strokeStyle=e.target.value;
+        }
+        if(e.target.id==='lineWidth'){
+            lineWidth=e.target.value;
+        }
+    });
+
+    const draw=(e)=>{
+        if(!isPainting){
+            return;
+        }
+    
+    ctx.linwWidth=lineWidth;
+    ctx.lineCap='round';
+    ctx.lineTo(e.clientX-canvasOffsetX,e.clientY);}
+   
+
+
+    canvas.addEventListener('mousedown',(e)=>{
+        isPaint=true;
+        startX=e.clientX;
+        startY=e.clientY;
+    });
+
+    canvas.addEventListener('mouseup',e=>{
+        isPainting=false;
+        ctx.stroke();
+        ctx.beginPath();
+    });
+
+    canvas.addEventListener('mousemove',draw);
+        
+    
